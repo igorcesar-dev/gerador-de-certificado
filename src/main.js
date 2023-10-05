783601dce6de0e711a3e876ac13e3f6d2df2aef9
igorcesar-dev <igorcesar.dev@gmail.com>
2023-10-05 01:35:05 -0300

    melhorando o desempenho do projeto

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import("../node_modules/bootstrap/dist/css/bootstrap.css")
-import("../node_modules/bootstrap/dist/js/bootstrap.js")
-import("../node_modules/html2pdf.js")
-import("../node_modules/xlsx/xlsx.js")
 
 
 createApp(App).use(router).mount('#app')
