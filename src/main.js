8d5d65aa185fddbde0776db57669dd2e70b83c0a
igorcesar-dev <igorcesar.dev@gmail.com>
2023-10-07 09:02:02 -0300

    Projeto versão 1.2

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import("../node_modules/bootstrap/dist/css/bootstrap.css")
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 createApp(App).use(router).mount('#app')
